6c0ec4620ba4b1b4cbf64c412791be04a536dfde
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-02-28 06:27:00 +0000

    made negascout() more or less efficient?  ;-)

--- trunk/src/search.cpp
+++ trunk/src/search.cpp
@@ -374,13 +374,10 @@ move_t search::negascout(int depth, int alpha, int beta)
 			alpha = it->value;
 			type = EXACT;
 		}
+		if (it == l.begin() || it->value > m.value)
+			m = *it;
 	}
 
-	/* Find the best move in the list.  This loop seems wasteful.  Can
-	 * anyone think of a better way? */
-	for (m = l.front(), it = l.begin(), it++; it != l.end(); it++)
-		if (it->value > m.value)
-			m = *it;
 	table_ptr->store(hash, m, depth, type);
 	history_ptr->store(whose, m, depth);
 	return m;
