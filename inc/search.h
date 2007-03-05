5b61ce75724f4a24f85b2c5b2f54b8ee33db5a8b
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-03-05 11:56:05 +0000

    removed broken quiescence search  :-(

--- inc/search.h
+++ inc/search.h
@@ -76,7 +76,6 @@ private:
 
 	void iterate(int s);
 	move_t negascout(int depth, int alpha, int beta);
-	int quiesce(int alpha, int beta);
 	void extract(int s);
 	static bool compare(move_t m1, move_t m2);
 };
