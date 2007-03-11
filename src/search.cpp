cd75902260f2a1319a2bcf3152fc5c793764c010
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-03-11 09:00:03 +0000

    reduced castling penalty, bonus

--- src/search.cpp
+++ src/search.cpp
@@ -433,7 +433,7 @@ move_t search::negascout(int depth, int alpha, int beta)
 		{
 			/* Recursive case: minimal (scout) window. */
 			if (type == EXACT)
-				it->value = -negascout(depth - 1, -alpha - WEIGHT_PAWN, -alpha).value;
+				it->value = -negascout(depth - 1, -alpha - WEIGHT_INCREMENT, -alpha).value;
 			/* Recursive case: full alpha-beta window. */
 			if (type != EXACT || alpha < it->value && it->value < beta)
 				it->value = -negascout(depth - 1, -beta, -alpha).value;
