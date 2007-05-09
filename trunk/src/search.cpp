fca80a2b8075fab235f56d768822f8d2006b5c85
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-05-09 23:20:56 +0000

    made pondering stronger?

--- trunk/src/search.cpp
+++ trunk/src/search.cpp
@@ -43,6 +43,7 @@ search::search(table *t, history *h, xboard *x)
 
 /* Constructor. */
 
+	SET_NULL_MOVE(hint);
 	max_time = INT_MAX;
 	max_depth = DEPTH;
 	output = false;
@@ -275,6 +276,8 @@ void search::iterate(int s)
 	clock_t start = clock();
 	if (s == THINKING)
 		timer_set(max_time);
+	if (s == PONDERING)
+		b.make(hint);
 
 	/*
 	 | Perform iterative deepening until the alarm has sounded (if we're
@@ -311,6 +314,8 @@ void search::iterate(int s)
 	 */
 	if (s == THINKING)
 		timer_cancel();
+	if (s == PONDERING)
+		b.unmake();
 	if (pv.front().value == (THINKING ? -WEIGHT_KING : WEIGHT_KING))
 		xboard_ptr->print_resignation();
 	if (s == THINKING && search_status != QUITTING)
@@ -503,15 +508,16 @@ void search::extract(int s)
 		b.unmake();
 
 	/* Get the hint. */
-	if (s == THINKING && pv.size() >= 2)
+	if (s == THINKING)
 	{
-		list<move_t>::iterator it = pv.begin();
-		hint = *++it;
+		if (pv.size() >= 2)
+		{
+			list<move_t>::iterator it = pv.begin();
+			hint = *++it;
+		}
+		else
+			SET_NULL_MOVE(hint);
 	}
-	else if (s == PONDERING && pv.size() >= 1)
-		hint = pv.front();
-	else
-		SET_NULL_MOVE(hint);
 }
 
 /*----------------------------------------------------------------------------*\
