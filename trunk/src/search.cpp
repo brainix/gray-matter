04f15e85e264e1cc12a82457035eb0d9bc8a0e6e
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-06-25 17:31:29 +0000

    undid brain damage

--- trunk/src/search.cpp
+++ trunk/src/search.cpp
@@ -431,14 +431,24 @@ move_t search::minimax(int depth, int shallowness, int alpha, int beta)
 		 |     leads to this position as +WEIGHT_KING.
 		 */
 		SET_NULL_MOVE(m);
-		m.value = status == IN_PROGRESS ? -b.evaluate() : +WEIGHT_CONTEMPT;
+		switch (status)
+		{
+			case IN_PROGRESS  : m.value = -b.evaluate();    break;
+			case STALEMATE    : m.value = +WEIGHT_CONTEMPT; break;
+			case INSUFFICIENT : m.value = +WEIGHT_CONTEMPT; break;
+			case THREE        : m.value = +WEIGHT_CONTEMPT; break;
+			case FIFTY        : m.value = +WEIGHT_CONTEMPT; break;
+			case CHECKMATE    : m.value = -WEIGHT_KING;     break;
+			case ILLEGAL      : m.value = -WEIGHT_KING;     break;
+		}
 		return m;
 	}
 
 	/*
-	 | If we've already sufficiently examined this position, return the best
-	 | move from our previous search.  Otherwise, if we can, reduce the size
-	 | of our alpha-beta window.
+	 | Before anything else, do some Research Re: search & Research.  ;-)
+	 | (Apologies to Aske Plaat.)  If we've already sufficiently examined
+	 | this position, return the best move from our previous search.
+	 | Otherwise, if we can, reduce the size of our alpha-beta window.
 	 */
 	if (shallowness)
 	{
@@ -454,8 +464,28 @@ move_t search::minimax(int depth, int shallowness, int alpha, int beta)
 		beta = LESSER(beta, upper);
 	}
 
-	/* Generate and re-order the move list. */
+	/* Generate the move list. */
 	b.generate(l, !shallowness);
+	if (l.empty())
+	{
+		/*
+		 | In this position, there are no legal moves.  The game must be
+		 | stalemated or checkmated.
+		 */
+		switch (b.get_status(true))
+		{
+			case IN_PROGRESS  : m.value = -b.evaluate();    break;
+			case STALEMATE    : m.value = +WEIGHT_CONTEMPT; break;
+			case INSUFFICIENT : m.value = +WEIGHT_CONTEMPT; break;
+			case THREE        : m.value = +WEIGHT_CONTEMPT; break;
+			case FIFTY        : m.value = +WEIGHT_CONTEMPT; break;
+			case CHECKMATE    : m.value = -WEIGHT_KING;     break;
+			case ILLEGAL      : m.value = -WEIGHT_KING;     break;
+		}
+		return m;
+	}
+
+	/* Re-order the move list. */
 	for (it = l.begin(); it != l.end(); it++)
 		/*
 		 | According to the transposition table, a previous search from
