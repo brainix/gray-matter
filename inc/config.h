bbdc62886d5a7799df7723b8f8e91689d8a3d9f3
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-08-06 21:02:14 +0000

    upgraded to GPL 3.0

--- inc/config.h
+++ inc/config.h
@@ -16,11 +16,7 @@
  | details.
  |
  | You should have received a copy of the GNU General Public License along with
- | this program; if not, write to:
- |
- |	The Free Software Foundation, Inc.
- |	59 Temple Place, Suite 330
- |	Boston MA 02111-1307
+ | this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 /* Windows insists on feeling included by being included first. */
