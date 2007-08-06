4b1ea3e0e9f7b5015c879e4a6f1168eed06784a9
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-08-06 21:27:04 +0000

    upgraded to GPL 3.0

--- trunk/inc/book.h
+++ trunk/inc/book.h
@@ -5,7 +5,7 @@
 \*----------------------------------------------------------------------------*/
 
 /*
- | This program is Free Software; you can redistribute it and/or modify it under
+ | This program is free software: you can redistribute it and/or modify it under
  | the terms of the GNU General Public License as published by the Free Software
  | Foundation; either version 3 of the License, or (at your option) any later
  | version.
@@ -16,7 +16,7 @@
  | details.
  |
  | You should have received a copy of the GNU General Public License along with
- | this program; if not, see <http://www.gnu.org/licenses/>.
+ | this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef BOOK_H
