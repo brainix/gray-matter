3cb480145d8bf288b4b87a894f020b7783941603
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-08-26 02:56:24 +0000

    implemented null move pruning?

--- trunk/src/search_mtdf.cpp
+++ trunk/src/search_mtdf.cpp
@@ -254,7 +254,7 @@ move_t search_mtdf::minimax(int depth, int shallowness, int alpha, int beta, boo
 		SET_NULL_MOVE(null_move);
 		null_move.value = +WEIGHT_ILLEGAL;
 		board_ptr->make(null_move);
-		null_move = minimax(depth - 3, shallowness + 3, -beta, -beta + 1, false);
+		null_move = minimax(depth - 4, shallowness + 4, -beta, -beta + 1, false);
 		board_ptr->unmake();
 		if ((null_move.value *= -1) >= beta)
 			return null_move;
