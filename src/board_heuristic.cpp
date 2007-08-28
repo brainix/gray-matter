bdb979a51d44133fda7296cc372e4535de7eb9b4
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-08-28 13:23:36 +0000

    reverted to previous (broken) version  :-(

--- src/board_heuristic.cpp
+++ src/board_heuristic.cpp
@@ -224,6 +224,9 @@ int board_heuristic::evaluate() const
 		return WEIGHT_ILLEGAL;
 
 	sum += evaluate_material();
+	sum += evaluate_position();
+	sum += evaluate_pawn();
+	sum += evaluate_king();
 	return sum;
 }
 
