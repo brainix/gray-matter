013202816fd804be9100ac04fdc633742db3ae87
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-09-03 14:33:45 +0000

    fixed bug

--- src/board_base.cpp
+++ src/board_base.cpp
@@ -1369,9 +1369,8 @@ bool board_base::check(bitboard_t b1, bool color) const
 		// a pawn.  Easy, breezy, beautiful.
 		bitboard_t pawn_attacks = 0;
 		for (int j = x == 0 ? 1 : -1; j <= (x == 7 ? -1 : 1); j += 2)
-			pawn_attacks |= ROW_MSK(x + j);
-		pawn_attacks &= !color ? y <= 1 ? 0 : COL_MSK(y - 1) :
-		                         y >= 6 ? 0 : COL_MSK(y + 1);
+			pawn_attacks |= COL_MSK(x + j);
+		pawn_attacks &= (!color && y <= 1 || color && y >= 6) ? 0 : ROW_MSK(y + (!color ? -1 : 1));
 		if (pawn_attacks & state.piece[color][PAWN])
 			return true;
 	}
