4cecfe75f0ba313d6d95eae69d4deb8d8b233b1a
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2007-11-10 11:37:25 +0000

    switched bzero() to memset() for portability

--- src/xboard.cpp
+++ src/xboard.cpp
@@ -78,7 +78,7 @@ void xboard::loop(search_base *s, chess_clock *c, book *o)
 
 	do
 	{
-		bzero(buffer, 80);
+		memset(buffer, '\0', 80);
 		fgets(buffer, 80, stdin);
 		if (!strncmp(buffer, "xboard", 6))
 			do_xboard();
