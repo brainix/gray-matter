fbf3885d042dfbd11da64907da45804fb2441230
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2008-03-22 05:58:34 +0000

    created bitboard class

---
+++ src/bitboard.cpp
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*\
+ |	bitboard.cpp - BitBoard implementation				      |
+ |									      |
+ |	Copyright © 2005-2008, The Gray Matter Team, original authors.	      |
+\*----------------------------------------------------------------------------*/
+
+/*
+ | This program is free software: you can redistribute it and/or modify it under
+ | the terms of the GNU General Public License as published by the Free Software
+ | Foundation, either version 3 of the License, or (at your option) any later
+ | version.
+ |
+ | This program is distributed in the hope that it will be useful, but WITHOUT
+ | ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ | FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ | details.
+ |
+ | You should have received a copy of the GNU General Public License along with
+ | this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "gray.h"
+#include "bitboard.h"
+
+/*----------------------------------------------------------------------------*\
+ |				   bitboard()				      |
+\*----------------------------------------------------------------------------*/
+bitboard::bitboard(int a, uint64_t d)
+{
+	angle = a;
+	data = d;
+}
