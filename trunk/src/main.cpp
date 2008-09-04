ada16f22aa73445d130a5662069365c7957a6e7e
brainix <brainix@5eabf372-9221-0410-879e-79e24070b877>
2008-09-04 01:42:15 +0000

    converted tabs to spaces

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*\
- |	main.cpp							      |
- |									      |
- |	Copyright © 2005-2008, The Gray Matter Team, original authors.	      |
+ |  main.cpp                                                                  |
+ |                                                                            |
+ |  Copyright © 2005-2008, The Gray Matter Team, original authors.            |
 \*----------------------------------------------------------------------------*/
 
 /*
@@ -45,109 +45,101 @@ int main(int argc, char **argv)
 {
     // First parse the configuration file, if exists
     ConfigFile config;
-	config.dump(cout);
+    config.dump(cout);
 
-	// Parse the command-line arguments and possibly change the default
-	// settings.
-	for (int c; (c = getopt(argc, argv, "e:x:n:m:o:p:")) != -1;)
-		switch (c)
-		{
-			case 'e':
-				// Specifying which move search engine to use.
-				if (optarg != "MTD(f)")
-				{
-					cerr << "move search engine "
-					     << "must be MTD(f)"
-					     << endl;
-					exit(EXIT_FAILURE);
-				}
-				config.set("engine", optarg);
-				break;
-			case 'x':
-				// Specifying the size of the transposition
-				// table.
-				if (atoi(optarg) < 1)
-				{
-					cerr << "transposition table "
-					     << "must be >= 1 MB"
-					     << endl;
-					exit(EXIT_FAILURE);
-				}
-				config.set("xpos_table_mb", atoi(optarg));
-				break;
-			case 'n':
-				// Specifying the file name of the opening book.
-				config.set("book_name", optarg);
-				break;
-			case 'm':
-				// Specifying the number of moves to read per
-				// game in the opening book.
-				if (atoi(optarg) < 1)
-				{
-					cerr << "number of book moves "
-					     << "must be >= 1 ply"
-					     << endl;
-					exit(EXIT_FAILURE);
-				}
-				config.set("book_moves", atoi(optarg));
-				break;
-			case 'o':
-				// Specifying the move search overhead.
-				if (atoi(optarg) < 1)
-				{
-					cerr << "move search overhead "
-					     << "must be >= 1 centisecond"
-					     << endl;
-					exit(EXIT_FAILURE);
-				}
-				config.set("overhead", atoi(optarg));
-				break;
-			case 'p':
-			{
-				// Specifying correctness and performance tests.
-				testing t(optarg);
-				t.start();
-				break;
-			}
-			default:
-				// Specifying the user doesn't know how to read.
-				cerr << "unknown option: -" 
-				     << static_cast<char>(optopt)
-				     << endl;
-				exit(EXIT_FAILURE);
-				break;
-		}
-	if (optind < argc)
-	{
-		// Specifying the user doesn't know how to read.
-		cerr << "unknown argument: " << argv[optind] << endl;
-		exit(EXIT_FAILURE);
-	}
+    // Parse the command-line arguments and possibly change the default
+    // settings.
+    for (int c; (c = getopt(argc, argv, "e:x:n:m:o:p:")) != -1;)
+        switch (c)
+        {
+            case 'e':
+                // Specifying which move search engine to use.
+                if (optarg != "MTD(f)")
+                {
+                    cerr << "move search engine must be MTD(f)" << endl;
+                    exit(EXIT_FAILURE);
+                }
+                config.set("engine", optarg);
+                break;
+            case 'x':
+                // Specifying the size of the transposition table.
+                if (atoi(optarg) < 1)
+                {
+                    cerr << "transposition table must be >= 1 MB" << endl;
+                    exit(EXIT_FAILURE);
+                }
+                config.set("xpos_table_mb", atoi(optarg));
+                break;
+            case 'n':
+                // Specifying the file name of the opening book.
+                config.set("book_name", optarg);
+                break;
+            case 'm':
+                // Specifying the number of moves to read per game in the
+                // opening book.
+                if (atoi(optarg) < 1)
+                {
+                    cerr << "number of book moves "
+                         << "must be >= 1 ply"
+                         << endl;
+                    exit(EXIT_FAILURE);
+                }
+                config.set("book_moves", atoi(optarg));
+                break;
+            case 'o':
+                // Specifying the move search overhead.
+                if (atoi(optarg) < 1)
+                {
+                    cerr << "move search overhead must be >= 1 centisecond"
+                         << endl;
+                    exit(EXIT_FAILURE);
+                }
+                config.set("overhead", atoi(optarg));
+                break;
+            case 'p':
+            {
+                // Specifying correctness and performance tests.
+                testing t(optarg);
+                t.start();
+                break;
+            }
+            default:
+                // Specifying that the user doesn't know how to read.
+                cerr << "unknown option: -" static_cast<char>(optopt) << endl;
+                exit(EXIT_FAILURE);
+                break;
+        }
+    if (optind < argc)
+    {
+        // Specifying that the user doesn't know how to read.
+        cerr << "unknown argument: " << argv[optind] << endl;
+        exit(EXIT_FAILURE);
+    }
 
-	// Seed the random number generator.
-	srand(time(NULL));
+    // Seed the random number generator.
+    srand(time(NULL));
 
-	// Instantiate the classes.
-	/// Transposition table object.
-	table t(config.getInt("xpos_table_mb"));
-	/// History table object.
-	history h;
-	/// Chess clock object.
-	chess_clock c(config.getInt("overhead"));
-	/// XBoard object.
-	xboard x;
-	/// Opening book object.
-	book o(&t, config.getString("book_name"), 
-	           config.getInt("book_moves"));
+    // Instantiate the classes.
+    /// Transposition table object.
+    table t(config.getInt("xpos_table_mb"));
+    /// History table object.
+    history h;
+    /// Chess clock object.
+    chess_clock c(config.getInt("overhead"));
+    /// XBoard object.
+    xboard x;
+    /// Opening book object.
+    book o(&t, config.getString("book_name"), 
+               config.getInt("book_moves"));
 
-	// Based on the -s command-line option, choose the move search engine
-	// and cast it as a generic version.  Thus far, we've only implemented
-	// one move search engine, MTD(f).
-	search_base *s = 0;
-	if (config.getString("engine") == "MTD(f)")
-		s = new search_mtdf(&t, &h, &c, &x);
+    // Based on the -s command-line option, choose the move search engine and
+    // cast it as a generic version.  Thus far, we've only implemented one move
+    // search engine, MTD(f).
+    search_base *s = 0;
+    if (config.getString("engine") == "MTD(f)")
+        s = new search_mtdf(&t, &h, &c, &x);
 
-	// Launch the event loop.
-	x.loop(s, &c, &o);
-	return 0;
+    // Launch the event loop.
+    x.loop(s, &c, &o);
+    return 0;
 }
